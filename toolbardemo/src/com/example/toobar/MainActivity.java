ccaf7e96aaf9145113338f6c136c1b91dfd7f838
liulei9385 <457667329@qq.com>
2015-01-19 09:56:31 +0800

    update

--- toolbardemo/src/com/example/toobar/MainActivity.java
+++ toolbardemo/src/com/example/toobar/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.toobar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.demo_toolbar);
+        toolbar.setTitle("hello world!");
     }
 }
