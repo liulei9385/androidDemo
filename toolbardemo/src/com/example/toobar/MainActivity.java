88f097d8a483b03658528c64723925dd40c64d2a
liulei9385 <457667329@qq.com>
2015-01-19 13:24:42 +0800

    update

--- toolbardemo/src/com/example/toobar/MainActivity.java
+++ toolbardemo/src/com/example/toobar/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.toobar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.Fragment;
+import android.support.v4.internal.view.SupportMenuItem;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import com.example.toobar.adapter.ViewPager_Adapter;
 import com.example.toobar.fragment.Fragment_Tab_1;
 import com.example.toobar.fragment.Fragment_Tab_2;
@@ -49,5 +56,41 @@ public class MainActivity extends ActionBarActivity {
         // 设置SlidingTab
         slidingTabLayout.setViewPager(viewPager);
 
+    }
+
+    //建立actionBar的选项菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        SupportMenuItem menuItem = (SupportMenuItem) menu.add(0, 1, 1, "settings");
+        menuItem.setShowAsAction(MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int itemId = item.getItemId();
+        switch (itemId) {
+            case 1:
+                showToast(itemId + " ", true);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showToast(final String text, boolean isLong) {
+        int duration = Toast.LENGTH_SHORT;
+        if (isLong)
+            duration = Toast.LENGTH_LONG;
+
+        final int duration2 = duration;
+        if (Looper.getMainLooper().getThread() == Thread.currentThread())
+            Toast.makeText(this, text, duration).show();
+        else new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @SuppressWarnings("ResourceType")
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, text, duration2).show();
+            }
+        });
     }
 }
