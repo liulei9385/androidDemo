1d30652b91629667b28f120f6cef5504bc9d8e26
liulei9385 <457667329@qq.com>
2015-01-19 11:29:15 +0800

    update

--- toolbardemo/src/com/example/toobar/MainActivity.java
+++ toolbardemo/src/com/example/toobar/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.toobar;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import com.example.toobar.adapter.ViewPager_Adapter;
+import com.example.toobar.fragment.Fragment_Tab_1;
+import com.example.toobar.fragment.Fragment_Tab_2;
+import com.example.toobar.fragment.Fragment_Tab_3;
+import com.example.toobar.view.SlidingTabLayout;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
+
+    private ArrayList<Fragment> fragments;
+    private ViewPager_Adapter viewPager_Adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Toolbar toolbar = (Toolbar) this.findViewById(R.id.demo_toolbar);
-        toolbar.setTitle("hello world!");
+        findView();
+    }
+
+    //初始化控件
+    private void findView() {
+        toolbar = (Toolbar) findViewById(R.id.demo_toolbar);
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.demo_tab);
+        viewPager = (ViewPager) findViewById(R.id.demo_vp);
+
+        // 实例化控件
+        fragments = new ArrayList<Fragment>();
+        fragments.add(new Fragment_Tab_1());
+        fragments.add(new Fragment_Tab_2());
+        fragments.add(new Fragment_Tab_3());
+        viewPager_Adapter = new ViewPager_Adapter(getSupportFragmentManager(), fragments);
+        viewPager.setOffscreenPageLimit(fragments.size());
+        viewPager.setAdapter(viewPager_Adapter);
+        // 设置SlidingTab
+        slidingTabLayout.setViewPager(viewPager);
     }
 }
